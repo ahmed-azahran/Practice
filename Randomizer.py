a744ed1e80fd63d088d4673521295518b7a669d5
Ahmed Zahran <45403060+ahmed-azahran@users.noreply.github.com>
2019-02-27 19:13:02 -0700

    Delete Randomizer.py

--- Randomizer.py
+++
@@ -1,8 +0,0 @@
-import random
-
-numbers = input("Enter the list of numbers separated by commas: ")
-num_list = numbers.split(",")
-secure_random = random.SystemRandom()
-random_num = secure_random.choice(num_list)
-print("Your random number is: ", random_num)
-
